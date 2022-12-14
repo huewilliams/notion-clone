0aadb1ec6cebfc7f7b238e069a26205182223d71
huewilliams <hswspwgod@gmail.com>
2022-12-14 22:58:24 +0900

    test: react testing library setting

--- frontend/apps/web/jest.config.js
+++ frontend/apps/web/jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTest.js'],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)?$",
+  transform: {
+    "^.+\\.tsx?$": ["ts-jest", {
+      tsconfig: 'tsconfig.jest.json'
+    }],
+  },
+}
