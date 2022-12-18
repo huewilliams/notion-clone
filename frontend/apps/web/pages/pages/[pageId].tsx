794fedeb82b9054fff9cce7b99435dfc1de3e62e
huewilliams <hswspwgod@gmail.com>
2022-12-18 20:37:45 +0900

    [web] feat: add Emoji and Title to Detail Page

--- frontend/apps/web/pages/pages/[pageId].tsx
+++ frontend/apps/web/pages/pages/[pageId].tsx
@@ -5,14 +5,36 @@ export default function Page() {
   return (
     <>
       <Banner>
-        <Image src={"/images/city.jpg"} alt={"city banner"} fill style={{objectFit: "cover"}}/>
+        <Image src={"/images/santorini.jpg"} alt={"santorini banner"} fill style={{objectFit: "cover"}}/>
       </Banner>
+      <Wrapper>
+        <Emoji>📄</Emoji>
+        <Title>Initial Page</Title>
+      </Wrapper>
     </>
   )
 }
 
 const Banner = styled.div`
   position: relative;
+  user-select: none;
   width: 100%;
   height: 200px;
+`;
+
+const Wrapper = styled.div`
+  padding: 60px 15%;
+`;
+
+const Emoji = styled.div`
+  position: absolute;
+  user-select: none;
+  top: 160px;
+
+  font-size: 56px;
+`;
+
+const Title = styled.h1`
+  font-size: 56px;
+  font-weight: 600;
 `;
