9ec5c5037f1e9fd7316753c1b7091050332b57c1
huewilliams <hswspwgod@gmail.com>
2022-12-29 01:14:57 +0900

    [editor] add: apply ProseMirror baseKeymap to Editor

--- frontend/packages/editor/Editor.tsx
+++ frontend/packages/editor/Editor.tsx
@@ -2,13 +2,20 @@
 import React, {useEffect, useRef} from "react";
 import {EditorState} from "prosemirror-state";
 import {EditorView} from "prosemirror-view";
+import {keymap} from "prosemirror-keymap";
+import {baseKeymap} from "prosemirror-commands";
 import {css} from "@emotion/react";
 import {schema} from "./model/schema";
 
 export function Editor() {
   const editorRef = useRef<HTMLDivElement>(null);
 
-  const state = EditorState.create({schema});
+  const state = EditorState.create({
+    schema,
+    plugins: [
+      keymap(baseKeymap)
+    ]
+  });
 
   useEffect(() => {
     const view = new EditorView(editorRef.current, {state});
