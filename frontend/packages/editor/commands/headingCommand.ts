cb7937c241f9f515197c9a7c17750f0534df5bf2
huewilliams <hswspwgod@gmail.com>
2022-12-30 01:25:21 +0900

    [editor] fix: type error

--- frontend/packages/editor/commands/headingCommand.ts
+++ frontend/packages/editor/commands/headingCommand.ts
@@ -8,22 +8,4 @@ export const headingCommand: Command = (state, dispatch) => {
     dispatch(tr);
   }
   return false;
-  const prevTextContent = state.selection.$head.parent.textContent;
-  const isHeadingCommand = prevTextContent.split('').every(c => c === "#");
-
-  if (prevTextContent.length < 4 && isHeadingCommand && dispatch) {
-    const from = state.selection.$head.pos - prevTextContent.length;
-    const to = state.selection.$head.pos;
-    const header = schema.nodes.heading.createAndFill({level: prevTextContent.length});
-
-    if (!header) return false;
-
-    const tr = state.tr;
-    tr.replaceWith(from - 1, to, header);
-    dispatch(tr);
-
-    return true;
-  }
-
-  return false;
 }
