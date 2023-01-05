588ca7877e4d43fc95d7c1dd47b02c5769673f69
huewilliams <hswspwgod@gmail.com>
2023-01-05 22:45:52 +0900

    [editor] add: blockquote command for android

--- frontend/packages/editor/android/wrapTransaction.ts
+++ frontend/packages/editor/android/wrapTransaction.ts
@@ -9,15 +9,26 @@ export function wrapTransaction(tr: Transaction, state: EditorState): Transactio
   const isWhiteSpaceContent = lastText && /\s/g.test(lastText);
   const isSpace = isWhiteSpaceContent && currentTextContent.length > prevTextContent.length;
   const isHeadingCommand = prevTextContent.length > 0 && prevTextContent.split('').every(c => c === "#");
+  const isBlockquoteCommand = prevTextContent === '"';
+
+  const from = state.selection.$head.pos - prevTextContent.length;
+  const to = state.selection.$head.pos;
 
   if (isSpace && isHeadingCommand) {
-    const from = state.selection.$head.pos - prevTextContent.length;
-    const to = state.selection.$head.pos;
     const header = schema.nodes.heading.createAndFill({level: prevTextContent.length});
 
     if (!header) return tr;
 
     tr.replaceWith(from - 1, to + 1, header);
+    tr.setSelection(new TextSelection(tr.doc.resolve(from - 1), tr.doc.resolve(from - 1)));
+  }
+
+  if (isSpace && isBlockquoteCommand) {
+    const blockquote = schema.nodes.blockquote.createAndFill();
+
+    if (!blockquote) return tr;
+
+    tr.replaceWith(from - 1, to + 1, blockquote);
     tr.setSelection(new TextSelection(tr.doc.resolve(from - 1), tr.doc.resolve(from - 1)));
   }
 
