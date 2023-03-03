3150df65ad940e1d0d1c310baf911ff6fc06c1af
huewilliams <hswspwgod@gmail.com>
2023-03-04 00:42:13 +0900

    [editor] fix: backtickCommand return when targetText empty

--- frontend/packages/editor/commands/backtickCommand.ts
+++ frontend/packages/editor/commands/backtickCommand.ts
@@ -2,8 +2,6 @@ import {Command} from "prosemirror-state";
 import {schema} from "../model/schema";
 
 export const backtickCommand: Command = (state, dispatch) => {
-  if (!dispatch) return false;
-
   const prevTextContent = state.selection.$head.parent.textContent;
   const currentPos = state.tr.selection.$head.pos;
   const textContentBeforeCurrent = prevTextContent.slice(0, currentPos - 1);
@@ -13,9 +11,10 @@ export const backtickCommand: Command = (state, dispatch) => {
 
   const from = prevTextContent.lastIndexOf("`") + 1;
   const {tr} = state;
-  const targetNode = prevTextContent.slice(from, currentPos);
+  const targetText = prevTextContent.slice(from, currentPos);
+  if (targetText.length < 1 || !dispatch) return false;
 
-  tr.replaceWith(from, currentPos, schema.text(targetNode));
+  tr.replaceWith(from, currentPos, schema.text(targetText));
   tr.addMark(from, currentPos, schema.marks.inlineCode.create());
   tr.removeStoredMark(schema.marks.inlineCode);
   dispatch(tr);
