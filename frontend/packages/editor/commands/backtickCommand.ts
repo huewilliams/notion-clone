f4c8b164380439d0efed4ef68fc0cc2206364d74
huewilliams <hswspwgod@gmail.com>
2023-03-04 12:38:16 +0900

    [editor] fix: backtickCommand apply startPos to from pos

--- frontend/packages/editor/commands/backtickCommand.ts
+++ frontend/packages/editor/commands/backtickCommand.ts
@@ -3,19 +3,20 @@ import {schema} from "../model/schema";
 
 export const backtickCommand: Command = (state, dispatch) => {
   const prevTextContent = state.selection.$head.parent.textContent;
-  const currentPos = state.tr.selection.$head.pos;
-  const textContentBeforeCurrent = prevTextContent.slice(0, currentPos - 1);
+  const {tr} = state;
+  const currentPos = tr.selection.$anchor.pos;
 
+  const textContentBeforeCurrent = prevTextContent.slice(0, currentPos - 1);
   const isBacktickExist = textContentBeforeCurrent.includes("`");
   if (!isBacktickExist) return false;
 
-  const from = prevTextContent.lastIndexOf("`") + 1;
-  const {tr} = state;
-  const targetText = prevTextContent.slice(from, currentPos);
+  const backtickOffset = prevTextContent.lastIndexOf("`");
+  const targetText = prevTextContent.slice(backtickOffset + 1, currentPos);
   if (targetText.length < 1 || !dispatch) return false;
 
-  tr.replaceWith(from, currentPos, schema.text(targetText));
-  tr.addMark(from, currentPos, schema.marks.inlineCode.create());
+  const startPos = tr.selection.$anchor.start();
+  tr.replaceWith(startPos + backtickOffset, currentPos, schema.text(targetText));
+  tr.addMark(startPos + backtickOffset, currentPos, schema.marks.inlineCode.create());
   tr.removeStoredMark(schema.marks.inlineCode);
   dispatch(tr);
 
