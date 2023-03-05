2c3254a2bff2747b83aef2d497a83988cf84c67a
huewilliams <hswspwgod@gmail.com>
2023-03-05 19:00:56 +0900

    [editor] add: divider nodeType to schema

--- frontend/packages/editor/model/schema.ts
+++ frontend/packages/editor/model/schema.ts
@@ -3,30 +3,29 @@ import {Schema} from "prosemirror-model";
 export const schema = new Schema({
   nodes: {
     doc: {
-      content: 'block+',
+      content: '(block | test)+',
     },
     paragraph: {
       content: 'inline*',
       group: 'block',
       parseDOM: [{tag: "p"}],
-      toDOM() {
-        return ['p', 0]
-      }
+      toDOM() { return ['p', 0] }
     },
     heading: {
       attrs: {level: {default: 1}},
       content: 'text*',
       group: 'block',
-      toDOM(node) {
-        return ['h' + node.attrs.level, 0]
-      }
+      toDOM(node) { return ['h' + node.attrs.level, 0] }
     },
     blockquote: {
       content: 'text*',
       group: 'block',
-      toDOM() {
-        return ['blockquote', {class: "editor-blockquote"}, 0]
-      }
+      toDOM() { return ['blockquote', {class: "editor-blockquote"}, 0] }
+    },
+    divider: {
+      group: 'test',
+      selectable: false,
+      toDOM() { return ['div', ['hr']] }
     },
     text: {
       group: 'inline'
