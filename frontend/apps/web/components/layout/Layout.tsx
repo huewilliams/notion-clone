fa613f0acebac5145154bfe6fc125819634cbd68
huewilliams <hswspwgod@gmail.com>
2023-11-11 23:51:57 +0900

    [web] add: Header to Layout

--- frontend/apps/web/components/layout/Layout.tsx
+++ frontend/apps/web/components/layout/Layout.tsx
@@ -1,10 +1,38 @@
 import {PropsWithChildren} from "react";
+import styled from "@emotion/styled";
+import Link from "next/link";
 
 export default function Layout({children}: PropsWithChildren) {
   return (
     <>
-      <div>Layout</div>
+      <Header>
+        <Link href={`/`}>
+          <LinkButton>
+            📓 Notion Clone
+          </LinkButton>
+        </Link>
+      </Header>
       <main>{children}</main>
     </>
   )
 }
+
+const Header = styled.div`
+  padding: 16px;
+
+  background: white;
+`;
+
+const LinkButton = styled.button`
+  padding: 4px 6px;
+
+  background: none;
+  border: none;
+  outline: none;
+  border-radius: 6px;
+  cursor: pointer;
+
+  &:hover {
+    background: #EBEBEB;
+  }
+`;
