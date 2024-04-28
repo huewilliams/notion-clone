5dcd9aea327195c0c840ead2708d988e3bbf124d
huewilliams <hswspwgod@gmail.com>
2024-04-28 12:12:35 +0900

    [web] add: GET documents/[id] api

---
+++ frontend/apps/web/pages/api/documents/[id].ts
@@ -0,0 +1,18 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {getDocs, query, collection, where} from "firebase/firestore";
+import {notionCloneFirestore} from "../../../firebase";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const {id} = req.query;
+
+    if (req.method === 'GET') {
+        const q = query(collection(notionCloneFirestore, "document"), where("id", "==", id));
+        const querySnapshot = await getDocs(q);
+
+        if (querySnapshot.docs.length < 1) res.status(404).end("Not Found");
+        res.json(querySnapshot.docs[0].data());
+        return;
+    }
+
+    res.end(`${id}`);
+}
